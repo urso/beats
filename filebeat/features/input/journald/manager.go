d478eb67c1fdc06ed9ad15055cce2800ac17171f
urso <steffen.siering@elastic.co>
2020-04-24 00:22:06 +0200

    fix init map

--- filebeat/features/input/journald/manager.go
+++ filebeat/features/input/journald/manager.go
@@ -1,20 +1,3 @@
-// Licensed to Elasticsearch B.V. under one or more contributor
-// license agreements. See the NOTICE file distributed with
-// this work for additional information regarding copyright
-// ownership. Elasticsearch B.V. licenses this file to you under
-// the Apache License, Version 2.0 (the "License"); you may
-// not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
-
 package journald
 
 import (
@@ -89,8 +72,8 @@ func (cim *CursorInputManager) Create(config *common.Config) (input.Input, error
 	cim.initOnce.Do(func() {
 		if cim.DefaultCleanTimeout <= 0 {
 			cim.DefaultCleanTimeout = 30 * time.Minute
-			cim.sessions = map[string]*session{}
 		}
+		cim.sessions = map[string]*session{}
 	})
 
 	settings := struct {
