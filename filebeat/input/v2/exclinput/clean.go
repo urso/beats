cd49bd34591baa07d35c48b58bffc1d5ffb0da36
Steffen Siering <steffen.siering@elastic.co>
2020-04-30 16:28:25 +0200

    add package exclinput

---
+++ filebeat/input/v2/exclinput/clean.go
@@ -0,0 +1,131 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package exclinput
+
+import (
+	"time"
+
+	"github.com/elastic/beats/v7/libbeat/logp"
+	"github.com/elastic/beats/v7/libbeat/statestore"
+	"github.com/elastic/go-concert/unison"
+)
+
+// cleaner removes finished entries from the registry file.
+type cleaner struct {
+	log *logp.Logger
+}
+
+// run starts a loop that tries to clean entries from the registry.
+// The cleaner locks the store, such that no new states can be created
+// during the cleanup phase. Only resources that are finished and whos TTL
+// (clean_timeout setting) has expired will be removed.
+//
+// Resources are considered "Finished" if they do not have a current owner (active input), and
+// if they have no pending updates that still need to be written to the registry file after associated
+// events have been ACKed by the outputs.
+// The event acquisition timestamp is used as reference to clean resources. If a resources was blocked
+// for a long time, and the life time has been exhausted, then the resource will be removed immediately
+// once the last event has been ACKed.
+func (c *cleaner) run(canceler unison.Canceler, store *store, interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	started := time.Now()
+
+	for {
+		select {
+		case <-ticker.C:
+			c.cleanup(started, store)
+		case <-canceler.Done():
+			return
+		}
+	}
+}
+
+// Cleanup looks for resources to remove and deletes these. `cleanup` receives
+// the start timestamp of the cleaner as reference. If we have entries without
+// updates in the registry, that are older than `started`, we will use `started
+// + ttl` to decide if an entry will be removed. This way old entries are not
+// removed immediately on startup if the Beat is down for a longer period of
+// time.
+func (c *cleaner) cleanup(started time.Time, store *store) {
+	c.log.Debugf("Start store cleanup")
+	defer c.log.Debugf("Done store cleanup")
+
+	states := store.ephemeralStore
+	states.mu.Lock()
+	defer states.mu.Unlock()
+
+	now := time.Now()
+
+	// keys stores all resource keys to delete. The bool indicates if the key was
+	// already written to the registry file, or if the resource is still in
+	// memory only (e.g. due to IO errors).
+	keys := map[string]bool{}
+	numStored := 0
+
+	for key, resource := range states.table {
+		if !resource.Finished() {
+			continue
+		}
+
+		ttl := resource.state.Internal.TTL
+		reference := resource.state.Internal.Updated
+		if started.After(reference) {
+			reference = started
+		}
+
+		if reference.Add(ttl).After(now) {
+			continue
+		}
+
+		keys[key] = resource.stored
+		if resource.stored {
+			numStored++
+		}
+	}
+
+	if len(keys) == 0 {
+		c.log.Debug("No entries to remove found")
+		return
+	}
+
+	// try to delete entries from the registry and internal state storage
+	if numStored > 0 {
+		err := store.persistentStore.Update(func(tx *statestore.Tx) error {
+			for k, stored := range keys {
+				if !stored {
+					continue
+				}
+
+				if err := tx.Remove(statestore.Key(k)); err != nil {
+					return err
+				}
+			}
+			return nil
+		})
+		if err != nil {
+			c.log.Errorf("Failed to remove entries from the registry: %+v", err)
+			return
+		}
+	}
+
+	for key := range keys {
+		delete(states.table, key)
+	}
+}
---
+++ filebeat/input/v2/exclinput/manager.go
@@ -0,0 +1,306 @@
+package exclinput
+
+import (
+	"context"
+	"fmt"
+	"runtime/debug"
+	"time"
+
+	"github.com/urso/sderr"
+
+	input "github.com/elastic/beats/v7/filebeat/input/v2"
+	v2 "github.com/elastic/beats/v7/filebeat/input/v2"
+	"github.com/elastic/beats/v7/libbeat/beat"
+	"github.com/elastic/beats/v7/libbeat/common"
+	"github.com/elastic/beats/v7/libbeat/common/acker"
+	"github.com/elastic/beats/v7/libbeat/logp"
+	"github.com/elastic/beats/v7/libbeat/statestore"
+
+	"github.com/elastic/go-concert/ctxtool"
+	"github.com/elastic/go-concert/unison"
+)
+
+type CursorInputManager struct {
+	Logger              *logp.Logger
+	StateStore          StateStore
+	Type                string
+	DefaultCleanTimeout time.Duration
+	Configure           func(cfg *common.Config) ([]Source, CursorInput, error)
+
+	session *session
+	store   *store
+}
+
+type StateStore interface {
+	Access() (*statestore.Store, error)
+	CleanupInterval() time.Duration
+}
+
+type beatsStore struct {
+	registry        *statestore.Registry
+	name            string
+	cleanupInterval time.Duration
+}
+
+func (s *beatsStore) Access() (*statestore.Store, error) {
+	return s.registry.Get(s.name)
+}
+
+func (s *beatsStore) CleanupInterval() time.Duration {
+	return s.cleanupInterval
+}
+
+type CursorInput interface {
+	Name() string
+	Test(Source, input.TestContext) error
+	Run(input.Context, Source, Cursor, Publisher) error
+}
+
+type Publisher interface {
+	Publish(event beat.Event, cursor interface{}) error
+}
+
+type Cursor struct {
+	session  *session
+	resource *resource
+}
+
+type Source interface {
+	Name() string
+}
+
+type managedInput struct {
+	manager      *CursorInputManager
+	sources      []Source
+	input        CursorInput
+	cleanTimeout time.Duration
+}
+
+type cursorPublisher struct {
+	ctx    *input.Context
+	client beat.Client
+	cursor *Cursor
+}
+
+func (cim *CursorInputManager) init() error {
+	if cim.DefaultCleanTimeout <= 0 {
+		cim.DefaultCleanTimeout = 30 * time.Minute
+	}
+
+	log := cim.Logger.With("input_type", cim.Type)
+	store, err := openStore(log, cim.StateStore, cim.Type)
+	if err != nil {
+		return err
+	}
+
+	cim.session = newSession(store)
+	cim.store = store
+
+	return nil
+}
+
+func (cim *CursorInputManager) Init(group unison.Group, mode v2.Mode) error {
+	if mode != v2.ModeRun {
+		return nil
+	}
+
+	if err := cim.init(); err != nil {
+		return err
+	}
+
+	log := cim.Logger.With("input_type", cim.Type)
+
+	store := cim.store
+	cleaner := &cleaner{log: log}
+	store.Retain()
+	err := group.Go(func(canceler unison.Canceler) error {
+		defer cim.shutdown()
+		defer store.Release()
+		cleaner.run(canceler, store, 1*time.Minute)
+		return nil
+	})
+	if err != nil {
+		store.Release()
+		cim.shutdown()
+		return sderr.Wrap(err, "Can not start registry cleanup process")
+	}
+
+	return nil
+}
+
+func (cim *CursorInputManager) shutdown() {
+	cim.session.Close()
+}
+
+func (cim *CursorInputManager) Create(config *common.Config) (input.Input, error) {
+	if err := cim.init(); err != nil {
+		return nil, err
+	}
+
+	settings := struct {
+		CleanTimeout time.Duration `config:"clean_timeout"`
+	}{CleanTimeout: cim.DefaultCleanTimeout}
+	if err := config.Unpack(&settings); err != nil {
+		return nil, err
+	}
+
+	sources, inp, err := cim.Configure(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &managedInput{
+		manager:      cim,
+		sources:      sources,
+		input:        inp,
+		cleanTimeout: settings.CleanTimeout,
+	}, nil
+}
+
+// Lock locks a key for exclusive access and returns an resource that can be used to modify
+// the cursor state and unlock the key.
+func (cim *CursorInputManager) lock(ctx input.Context, key string) (*resource, error) {
+	log := ctx.Logger
+
+	resource := cim.store.Find(key, true)
+	if !resource.lock.TryLock() {
+		log.Infof("Resource '%v' currently in use, waiting...", key)
+		err := resource.lock.LockContext(ctx.Cancelation)
+		if err != nil {
+			log.Infof("Input for resource '%v' has been stopped while waiting", key)
+			return nil, err
+		}
+	}
+	return resource, nil
+}
+
+func (inp *managedInput) Name() string { return inp.input.Name() }
+
+func (inp *managedInput) Test(ctx input.TestContext) error {
+	var grp unison.MultiErrGroup
+	for _, source := range inp.sources {
+		source := source
+		grp.Go(func() error { return inp.input.Test(source, ctx) })
+	}
+	if errs := grp.Wait(); len(errs) > 0 {
+		sderr.WrapAll(errs, "input tests failed")
+	}
+	return nil
+}
+
+func (inp *managedInput) Run(
+	ctx input.Context,
+	pipeline beat.PipelineConnector,
+) (err error) {
+	// Setup cancellation using a custom cancel context. All workers will be
+	// stopped if one failed badly.
+	cancelCtx, cancel := context.WithCancel(ctxtool.FromCanceller(ctx.Cancelation))
+	defer cancel()
+	ctx.Cancelation = cancelCtx
+
+	session := inp.manager.session
+	if err != nil {
+		sderr.Wrap(err, "failed to access the state store")
+	}
+
+	var grp unison.MultiErrGroup
+	for _, source := range inp.sources {
+		source := source
+		grp.Go(func() (err error) {
+			// refine per worker context
+			inpCtx := ctx
+			inpCtx.ID = ctx.ID + "::" + source.Name()
+			inpCtx.Logger = ctx.Logger.With("source", source.Name())
+
+			if err = inp.runSource(inpCtx, session, source, pipeline); err != nil {
+				cancel()
+			}
+			return err
+		})
+	}
+
+	if errs := grp.Wait(); len(errs) > 0 {
+		sderr.WrapAll(errs, "input %v failed", ctx.ID)
+	}
+	return nil
+}
+
+func (inp *managedInput) runSource(
+	ctx input.Context,
+	session *session,
+	source Source,
+	pipeline beat.PipelineConnector,
+) (err error) {
+	// Setup error recovery/reporting
+	defer func() {
+		if v := recover(); v != nil {
+			err = fmt.Errorf("input panic with: %+v\n%s", v, debug.Stack())
+			ctx.Logger.Errorf("Input crashed with: %+v", err)
+		}
+	}()
+
+	// connect to libbeat publisher pipeline
+	client, err := pipeline.ConnectWith(beat.ClientConfig{
+		CloseRef: ctx.Cancelation,
+		Processing: beat.ProcessingConfig{
+			DynamicFields: ctx.Metadata,
+		},
+		ACKHandler: newInputACKHandler(ctx.Logger, inp.manager),
+	})
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	// lock resource for exclusive access and create cursor
+	resource, err := inp.manager.lock(ctx, fmt.Sprintf("%v::%v", inp.manager.Type, source.Name()))
+	if err != nil {
+		return err
+	}
+	defer resource.Unlock()
+
+	// update clean timeout. If the resource is 'new' we will insert it into the registry now.
+	if resource.stored == false || inp.cleanTimeout != resource.state.Internal.TTL {
+		resource.state.Internal.TTL = inp.cleanTimeout
+		session.store.UpdateInternal(resource)
+	}
+
+	cursor := Cursor{session: session, resource: resource}
+	publisher := &cursorPublisher{ctx: &ctx, client: client, cursor: &cursor}
+	return inp.input.Run(ctx, source, cursor, publisher)
+}
+
+func (c *cursorPublisher) Publish(event beat.Event, cursorUpdate interface{}) error {
+	op, err := c.cursor.session.CreateUpdateOp(c.cursor.resource, cursorUpdate)
+	if err != nil {
+		return err
+	}
+
+	event.Private = op
+	c.client.Publish(event)
+	return c.ctx.Cancelation.Err()
+}
+
+func newInputACKHandler(log *logp.Logger, manager *CursorInputManager) beat.ACKer {
+	return acker.LastEventPrivateReporter(func(acked int, private interface{}) {
+		op, ok := private.(*updateOp)
+		if !ok {
+			log.Errorf("Wrong event type ACKed")
+		} else {
+			op.Execute()
+		}
+	})
+}
+
+func (c Cursor) IsNew() bool { return c.resource.IsNew() }
+
+func (c Cursor) Unpack(to interface{}) error {
+	if c.IsNew() {
+		return nil
+	}
+	return c.resource.UnpackCursor(to)
+}
+
+func (c Cursor) Migrate(val interface{}) error {
+	return c.session.store.Migrate(c.resource, val)
+}
