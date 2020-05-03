8aca55fe732bed22eea0bfdb00e0f3f53f3893a7
urso <steffen.siering@elastic.co>
2020-05-03 15:13:44 +0200

    ensure timestamp parsing if numbers are float (input has been read from json)

--- libbeat/common/transform/typeconv/typeconv.go
+++ libbeat/common/transform/typeconv/typeconv.go
@@ -120,29 +120,7 @@ func Convert(to, from interface{}) (err error) {
 }
 
 func foldTimestamp(in *time.Time, v structform.ExtVisitor) error {
-	var (
-		ts  = *in
-		off int16
-		loc = ts.Location()
-	)
-
-	const encodingVersion = 0
-
-	if loc == time.UTC {
-		off = -1
-	} else {
-		_, offset := ts.Zone()
-		offset /= 60 // Note: best effort. If the zone offset has a factional minute, then we will ignore it here
-		if offset < -32768 || offset == -1 || offset > 32767 {
-			offset = 0 // Note: best effort. Ignore offset if it becomes an unexpected value
-		}
-		off = int16(offset)
-	}
-
-	sec := uint64(ts.Unix())
-	extra := (uint64(encodingVersion) << 56) |
-		(uint64(uint16(off)) << 32) |
-		uint64(ts.Nanosecond())
+	extra, sec := timestampToBits(*in)
 
 	if err := v.OnArrayStart(2, structform.Uint64Type); err != nil {
 		return err
@@ -188,6 +166,12 @@ func (u *timeUnfolder) OnArrayStart(ctx gotype.UnfoldCtx, len int, _ structform.
 	return nil
 }
 
+func (u *timeUnfolder) OnInt(ctx gotype.UnfoldCtx, in int64) error {
+	return u.OnUint(ctx, uint64(in))
+}
+func (u *timeUnfolder) OnFloat(ctx gotype.UnfoldCtx, f float64) error {
+	return u.OnUint(ctx, uint64(f))
+}
 func (u *timeUnfolder) OnUint(ctx gotype.UnfoldCtx, in uint64) error {
 	switch u.st {
 	case timeUnfoldWaitA:
@@ -202,10 +186,6 @@ func (u *timeUnfolder) OnUint(ctx gotype.UnfoldCtx, in uint64) error {
 	return nil
 }
 
-func (u *timeUnfolder) OnInt(ctx gotype.UnfoldCtx, in int64) error {
-	return u.OnUint(ctx, uint64(in))
-}
-
 func (u *timeUnfolder) OnArrayFinished(ctx gotype.UnfoldCtx) error {
 	defer ctx.Done()
 
@@ -215,28 +195,64 @@ func (u *timeUnfolder) OnArrayFinished(ctx gotype.UnfoldCtx) error {
 
 	u.st = timeUnfoldDone
 
-	version := (u.a >> 56) & 0xff
-	if version != 0 {
-		return fmt.Errorf("invalid timestamp [%x, %x]", u.a, u.b)
+	ts, err := bitsToTimestamp(u.a, u.b)
+	if err != nil {
+		return err
+	}
+	*u.to = ts
+
+	return nil
+}
+
+func timestampToBits(ts time.Time) (uint64, uint64) {
+	var (
+		off int16
+		loc = ts.Location()
+	)
+
+	const encodingVersion = 0
+
+	if loc == time.UTC {
+		off = -1
+	} else {
+		_, offset := ts.Zone()
+		offset /= 60 // Note: best effort. If the zone offset has a factional minute, then we will ignore it here
+		if offset < -32768 || offset == -1 || offset > 32767 {
+			offset = 0 // Note: best effort. Ignore offset if it becomes an unexpected value
+		}
+		off = int16(offset)
 	}
 
-	sec := u.b
-	nsec := uint32(u.a)
-	off := int16(u.a >> 32)
+	sec := uint64(ts.Unix())
+	extra := (uint64(encodingVersion) << 56) |
+		(uint64(uint16(off)) << 32) |
+		uint64(ts.Nanosecond())
 
-	ts := u.to
-	*ts = time.Unix(int64(sec), int64(nsec))
+	return extra, sec
+}
+
+func bitsToTimestamp(extra, sec uint64) (time.Time, error) {
+	var ts time.Time
+
+	version := (extra >> 56) & 0xff
+	if version != 0 {
+		return ts, fmt.Errorf("invalid timestamp [%x, %x]", extra, sec)
+	}
+
+	nsec := uint32(extra)
+	off := int16(extra >> 32)
+	ts = time.Unix(int64(sec), int64(nsec))
 
 	// adjust location by offset. time.Unix creates a timestamp in the local zone
 	// by default. Only change this if off does not match the local zone it's offset.
 	if off == -1 {
-		*ts = ts.UTC()
+		ts = ts.UTC()
 	} else if off != 0 {
 		_, locOff := ts.Zone()
 		if off != int16(locOff/60) {
-			*ts = ts.In(time.FixedZone("", int(off*60)))
+			ts = ts.In(time.FixedZone("", int(off*60)))
 		}
 	}
 
-	return nil
+	return ts, nil
 }
