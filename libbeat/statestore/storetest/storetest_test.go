5f72b46c5b4f1f34a091780073e401ffe221b870
urso <steffen.siering@elastic.co>
2020-06-10 13:35:54 +0200

    fix missing rename

--- libbeat/statestore/storetest/storetest_test.go
+++ libbeat/statestore/storetest/storetest_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/elastic/beats/v7/libbeat/logp"
 	"github.com/elastic/beats/v7/libbeat/statestore/backend"
-	"github.com/elastic/beats/v7/libbeat/statestore/backend/storetests"
+	"github.com/elastic/beats/v7/libbeat/statestore/backend/storecompliance"
 )
 
 func init() {
@@ -13,7 +13,7 @@ func init() {
 }
 
 func TestCompliance(t *testing.T) {
-	storetests.TestBackendCompliance(t, func(testPath string) (backend.Registry, error) {
+	storecompliance.TestBackendCompliance(t, func(testPath string) (backend.Registry, error) {
 		return NewMemoryStoreBackend(), nil
 	})
 }
